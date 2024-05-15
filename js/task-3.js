45432efd042ab932fc3588838772c50ee8d4af90
Andrii Sukhovetskyi <andreysukhovetskyi@gmail.com>
2024-05-15 20:22:30 +0200

    updata

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function getElementWidth(content, padding, border) {
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding);
+  const borderWidth = parseFloat(border);
+  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+  return totalWidth;
+}
+
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
